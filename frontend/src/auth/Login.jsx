09e564609ec7d12fa60da4224b6ffcc97a7b04c5
isiosio <thugforsign@gmail.com>
2026-04-29 14:59:23 +0100

    update frontend api and login before migration

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import api from "../api";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
@@ -13,7 +13,7 @@ const Login = () => {
 
     try {
       // Send login request to backend
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await api.post("/auth/login", {
   email,
   password,
 });
